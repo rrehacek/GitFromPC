65c5b88796db825c9bd7a55e603b5526ba9485b7
radekrehacek <rrehacek10@gmail.com>
2019-04-15 21:03:55 +0200

    ByeBye for loop edited

--- GitTest.py
+++ GitTest.py
@@ -3,5 +3,9 @@ print("Hello world!")
 for i in range(0, 10):   #added the semicolon - thus fixing the bug
   print("Hello ",  i)
 
-for i in range(0, 10):
+print()
+
+for i in range(0, 5):   #lowered down the number of repetitions
   print("Bye bye ", i)
+
+print("Lesson finished. Congrats!")
